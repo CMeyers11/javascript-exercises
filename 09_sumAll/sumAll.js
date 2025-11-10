5a19a9f0aa9d46e6ffc81d443e294d1de5cf787e
Chad M <meyerschad11@gmail.com>
2025-11-10 03:03:58 -0500

    Attempt #1 for exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(bound1, bound2) {
+    const upperBound = Math.max(bound1,bound2);
+    const lowerBound = Math.min(bound1,bound2);
+    const quantity = upperBound - lowerBound + 1;
+    let sumInputs = 0;
+    for(let i = 0; i < quantity; i++)
+        sumInputs += lowerBound + i;
 };
 
 // Do not edit below this line
