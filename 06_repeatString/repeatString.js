0e10287b1c9722bc44b0872b43e7223cf3c4cccb
Chad M <meyerschad11@gmail.com>
2025-11-09 23:39:48 -0500

    First attempt exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function(inputString, repeatCount) {
+    let outputArray = []
+    for (i=0; i<repeatCount; i++) {
+        outputArray[i] = inputString;
+    }
+    return outputArray;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
