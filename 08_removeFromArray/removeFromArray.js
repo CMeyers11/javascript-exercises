42ea54c31a876d0c10aeca58eae6a4e6c734ce8b
Chad M <meyerschad11@gmail.com>
2025-11-10 02:52:52 -0500

    Complete exercise 08 and used ... to create argument with optional quantity

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput,...removedArgs) {
+    return arrayInput.filter(element => !removedArgs.includes(element));
 };
 
+//alert(removeFromArray([3,5,"a", 10], 2, "a", 3, "b"));
 // Do not edit below this line
 module.exports = removeFromArray;
