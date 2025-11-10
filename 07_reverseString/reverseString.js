bbb8d2ac9e0c234e1e9169d418acd2c830a255b2
Chad M <meyerschad11@gmail.com>
2025-11-10 02:17:42 -0500

    complete exercise 07 using for loop. Next attempt will use string methods

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
 
-};
-
+// alert(reverseString("Test"));
 // Do not edit below this line
 module.exports = reverseString;
+
+/* 
+Attempt #1 with for loop;
+attempt #2 above with string methods
+
+const reverseString = function(stringInput) {
+    let length = stringInput.length;
+    let stringOutput = ""
+    for (i=length-1; i>=0; i--) {
+        stringOutput += stringInput.charAt(i);
+    }
+    return stringOutput;
+}
+*/
