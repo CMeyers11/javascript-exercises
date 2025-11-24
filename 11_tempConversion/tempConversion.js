bc174f71b8a884b2eac83dc5f2c3085e1d00819b
Chad M <meyerschad11@gmail.com>
2025-11-23 23:44:28 -0500

    Create rounding function

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+function convertToCelsius(tempF) {
+  let tempC = (tempF - 32)*(5/9);
+  return roundTo(tempC,1);
 };
 
-const convertToFahrenheit = function() {
+function convertToFahrenheit(tempC) {
+  let tempF = (tempC *(9/5)) + 32;
+  return roundTo(tempF,1);
 };
+
+function roundTo(number, decimals) {
+  let factor = 10 ** decimals;
+  let step = (number * factor);
+  return Math.round(step)/factor;
+}
 
 // Do not edit below this line
 module.exports = {
